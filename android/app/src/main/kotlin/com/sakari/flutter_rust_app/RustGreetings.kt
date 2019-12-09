9b4c0e08b804c2559653250e8b6552e792c7e83a
Sheng Tang <ts25504@gmail.com>
2019-12-09 15:03:09 +0800

    Update RustGreetings.kt

--- android/app/src/main/kotlin/com/sakari/flutter_rust_app/RustGreetings.kt
+++ android/app/src/main/kotlin/com/sakari/flutter_rust_app/RustGreetings.kt
@@ -1,9 +1,5 @@
 package com.sakari.flutter_rust_app
 
-/**
- * Created by TangSheng (tangsheng02@meituan.com) on 2019/4/9.
- */
-
 class RustGreetings {
     private external fun greeting(pattern: String): String
 
